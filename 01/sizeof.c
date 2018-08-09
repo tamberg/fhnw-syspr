3093f0f06b7cfcb45f82ecf745a2d4f401fb0df0
Thomas Amberg <tamberg@yaler.net>
2018-08-09 13:02:08 +0200

    Removed redundant types from sizeof.c

--- 01/sizeof.c
+++ 01/sizeof.c
@@ -5,11 +5,8 @@ int main() {
     printf("sizeof(short) = %lu\n", sizeof(short));
     printf("sizeof(int) = %lu\n", sizeof(int));
     printf("sizeof(unsigned int) = %lu\n", sizeof(unsigned int));
-    printf("sizeof(long int) = %lu\n", sizeof(long int));
     printf("sizeof(long) = %lu\n", sizeof(long));
     printf("sizeof(unsigned long) = %lu\n", sizeof(unsigned long));
-    printf("sizeof(long long) = %lu\n", sizeof(long long));
-    printf("sizeof(long long int) = %lu\n", sizeof(long long int));
     printf("sizeof(float) = %lu\n", sizeof(float));
     printf("sizeof(double) = %lu\n", sizeof(double));
     return 0;
