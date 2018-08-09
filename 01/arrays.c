fc97b152b37e29ff7acbeaf225fe74900aa0baa6
Thomas Amberg <tamberg@yaler.net>
2018-08-09 14:16:03 +0200

    Fixed arrays.c, sizeof.c, added makefile.

--- 01/arrays.c
+++ 01/arrays.c
@@ -2,7 +2,7 @@
 
 int main() {
     //float temp_values[3];
-    float temp_values[2] = { 20.1, 23, 15.2 };
+    float temp_values[3] = { 20.1, 23, 15.2 };
     printf("%.1f\n", temp_values[1]);
     temp_values[1] =  7.0;
     printf("%.1f\n", temp_values[2]);
---
+++ 01/structs.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    struct point { int x; int y; };
+    struct point p = { 3, 2 };
+    struct point q;
+    q.x = p.y;
+    printf("(%d, %d)\n", p.x, p.y);
+    printf("(%d, %d)\n", q.x, q.y);
+    return 0;
+}
