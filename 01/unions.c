743ca5e1a636da5c775a87c22baf6683beb9af5f
Thomas Amberg <tamberg@yaler.net>
2018-08-10 14:05:01 +0200

    Cleanup.

--- 01/unions.c
+++ 01/unions.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
-// 0xEF => F, E => 1 1 1 1, 0 1 1 1 => 
-// Little Endian, confirmed by $ lscpu
+// 0xEF => F, E => 1 1 1 1, 0 1 1 1 => Little Endian
+// 0xEF => E, F => 1 1 1 0, 1 1 1 1 => Big Endian
+// Verify on Linux with $ lscpu
 
 int main() {
-	typedef union {
-	    unsigned char byte_value;
-	    struct bit_layout {
-	        unsigned int lo_nibble : 4;
-	        unsigned int hi_nibble_lsb : 1;
-	    } bit_values;
-	} Packet;
+    typedef union {
+        unsigned char byte_value;
+        struct bit_layout {
+            unsigned int lo_nibble : 4;
+            unsigned int hi_nibble_lsb : 1;
+        } bit_values;
+    } Packet;
 
-	Packet p;
-	p.byte_value = 0xEF;
-	printf("byte value 0x%x\n", p.byte_value);
-	printf("lo = %d\n", p.bit_values.lo_nibble);
-	printf("hi.lsb = %d\n", p.bit_values.hi_nibble_lsb);
+    Packet p;
+    p.byte_value = 0xEF;
+    printf("byte value 0x%x\n", p.byte_value);
+    printf("lo = %d\n", p.bit_values.lo_nibble);
+    printf("hi.lsb = %d\n", p.bit_values.hi_nibble_lsb);
     return 0;
 }
