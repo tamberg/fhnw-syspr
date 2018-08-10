75e53279a4b611c85997ca90f895931d188bef18
Thomas Amberg <tamberg@yaler.net>
2018-08-10 10:49:37 +0200

    Added unions.c

---
+++ 01/unions.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	typedef union {
+	    unsigned char byte_value;
+	    struct bit_layout {
+	        unsigned int lo_nibble : 4;
+//	        unsigned int hi_nibble : 4;
+	        unsigned int hi_nibble_lsb : 1;
+	    } bit_values;
+	} Packet;
+
+	Packet p;
+	p.byte_value = 0xEF;
+	printf("lo = %d\n", p.bit_values.lo_nibble);
+//	printf("hi = %d\n", p.bit_values.hi_nibble);
+	printf("hi.lsb = %d\n", p.bit_values.hi_nibble_lsb);
+    return 0;
+}
