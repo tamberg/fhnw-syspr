3f480f5b7621a4e6e13854518f75a4d02da31cd3
Thomas Amberg <tamberg@yaler.net>
2018-08-10 15:59:20 +0200

    Cleanup.

--- 01/limits.c
+++ 01/limits.c
@@ -4,17 +4,17 @@
 int main () {
     printf("Bits per byte %d\n", CHAR_BIT);
 
-    printf("char: %d <= c <= %d\n", CHAR_MIN, CHAR_MAX);
-    printf("signed char: %d <= c <= %d\n", SCHAR_MIN, SCHAR_MAX);
-    printf("unsigned char: %d <= c <= %d\n", UCHAR_MIN, UCHAR_MAX);
+    printf("char: %d .. %d\n", CHAR_MIN, CHAR_MAX);
+    printf("signed char: %d .. %d\n", SCHAR_MIN, SCHAR_MAX);
+    printf("unsigned char: 0 .. %d\n", UCHAR_MAX);
 
-    printf("short: %d <= s <= %d\n", SHRT_MIN, SHRT_MAX);
-    printf("unsigned short: %d <= c <= %d\n", USHRT_MIN, USHRT_MAX);
+    printf("short: %d .. %d\n", SHRT_MIN, SHRT_MAX);
+    printf("unsigned short: 0 .. %d\n", USHRT_MAX);
 
-    printf("int: %d <= s <= %d\n", INT_MIN, INT_MAX);
-    printf("unsigned int: %d <= c <= %d\n", UINT_MIN, UINT_MAX);
+    printf("int: %d .. %d\n", INT_MIN, INT_MAX);
+    printf("unsigned int: 0 .. %d\n", UINT_MAX);
 
-    printf("long: %d <= s <= %d\n", LONG_MIN, LONG_MAX);
-    printf("unsigned long: %d <= c <= %d\n", ULONG_MIN, ULONG_MAX);
+    printf("long: %ld .. %ld\n", LONG_MIN, LONG_MAX);
+    printf("unsigned long: 0 .. %ld\n", ULONG_MAX);
     return 0;
 }
