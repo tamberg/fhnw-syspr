f6e0e4fc9223383f8061a63f32c4814071a7f635
Thomas Amberg <tamberg@yaler.net>
2018-08-10 16:26:52 +0200

    Cleanup.

--- 01/limits.c
+++ 01/limits.c
@@ -2,21 +2,21 @@
 #include <limits.h>
 
 int main () {
-    printf("Bits per byte %d\n\n", CHAR_BIT);
-    
+    printf("bits per byte %d\n\n", CHAR_BIT);
+	// char
     printf("char: %d .. %d\n", CHAR_MIN, CHAR_MAX);
     printf("signed char: %d .. %d\n", SCHAR_MIN, SCHAR_MAX);
     printf("unsigned char: 0 .. %d\n\n", UCHAR_MAX);
-    
+	// short
     printf("short: %d .. %d\n", SHRT_MIN, SHRT_MAX);
     printf("unsigned short: 0 .. %d\n\n", USHRT_MAX);
-    
+    // int
     printf("int: %d .. %d\n", INT_MIN, INT_MAX);
     printf("unsigned int: 0 .. %u\n\n", UINT_MAX);
-    
+    // long
     printf("long: %ld .. %ld\n", LONG_MIN, LONG_MAX);
     printf("unsigned long: 0 .. %lu\n\n", ULONG_MAX);
-    
+    // long long (not defined on the Raspberry Pi Zero W)
     //printf("long long: %lld .. %lld\n", LONG_LONG_MIN, LONG_LONG_MAX);
     //printf("unsigned long long: 0 .. %llu\n", ULONG_LONG_MAX);
     return 0;
