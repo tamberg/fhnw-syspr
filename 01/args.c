e2691ff809116873eddb941b6e6b93a793eb813e
Thomas Amberg <tamberg@yaler.net>
2018-08-12 13:53:10 +0000

    Extended args.c to enforce [a-z]*.

--- 01/args.c
+++ 01/args.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[ ]) {
+    // check each arg, except program name
+    for (int i = 1; i < argc; i++) {
+        for (char *p = argv[i]; *p != '\0'; p++) {
+            if (*p < 'a' || *p > 'z') {
+                printf("error: args must contain [a-z]* only\n");
+                return -1;
+            }
+        }
+    }
+    // print all args
     for (int i = 0; i < argc; i++) {
-        printf("%d: %s, ", i, argv[i]);
+        printf("%d: %s\n", i, argv[i]);
     }
     return 0;
 }
