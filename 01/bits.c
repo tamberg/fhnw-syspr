d20944f82033d2e71887ad580dda8079ab7ceb97
Thomas Amberg <tamberg@yaler.net>
2018-08-14 14:24:02 +0200

    Added example values.

--- 01/bits.c
+++ 01/bits.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main (){
+    //unsigned long v = 18446744073709551615UL;
+    unsigned long v = 4294967295;
     //int v = -2147483648;
-    char v = 127;
+    //char v = 127;
     int n = sizeof(v);
     for (int i = n*8 - 1; i >= 0; i--) {
       int bit = (v >> i) & 0x1;
