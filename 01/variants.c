c0549c7c67d4ee8cf8391a2af29a52429843da3a
Thomas Amberg <tamberg@yaler.net>
2018-08-20 11:21:43 +0000

    Updated variants.c

--- 01/variants.c
+++ 01/variants.c
@@ -5,8 +5,8 @@ int main() {
         int int_value;
         float float_value;
     } Variant;
-    //Variant v = { 7 } // sets first (int_value)
-    //Variant v = { 23.0 } // does not set float_value
+    //Variant v = { 7 }; // sets first field, int_value
+    //Variant v = { 23.0 }; // does not set float_value
     //Variant v = { .float_value = 23.0 }; // c99 only
     Variant v; 
     v.int_value = 7;
