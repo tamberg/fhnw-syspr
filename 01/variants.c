53c1cec91b619abb2071a20a4e4045a25cd2bf42
Thomas Amberg <tamberg@yaler.net>
2018-08-20 09:02:31 +0000

    Added variants.c

---
+++ 01/variants.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    typedef union variant {
+        int int_value;
+        float float_value;
+    } Variant;
+    //Variant v = { 7 } // sets first (int_value)
+    //Variant v = { 23.0 } // does not set float_value
+    //Variant v = { .float_value = 23.0 }; // c99 only
+    Variant v; 
+    v.int_value = 7;
+    printf("%d\n", v.int_value);
+    v.float_value = 23.0;
+    printf("%f\n", v.float_value);
+}
