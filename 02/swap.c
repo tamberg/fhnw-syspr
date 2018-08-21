edff4ed4248637b60f7c2c4f6caf90f5feccb4e3
Thomas Amberg <tamberg@yaler.net>
2018-08-21 15:22:55 +0000

    Added swap.c

---
+++ 02/swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+int main() {
+    int x = 42, y = 23;
+    swap(&x, &y);
+    printf("%d\n", x);
+    printf("%d\n", y);
+}
