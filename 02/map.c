bbe2dc1e6292d8a0549112db9755c65fb8e2ea8e
Thomas Amberg <tamberg@yaler.net>
2018-08-21 19:13:04 +0000

    Fixed spaces.

--- 02/map.c
+++ 02/map.c
@@ -7,15 +7,15 @@ int inc(int i) {
 
 void map(int a[], int len, int (*f)(int)) {
     printf("map\n");
-    for (int i=0; i < len; i++) {
+    for (int i = 0; i < len; i++) {
         a[i] = f(a[i]); 
     }
 }
 
-void map(int a[], int len, int (*f)(int));
+//void map(int a[], int len, int (*f)(int));
 
 int main() {
     int a[] = {1, 2, 3};
-    map(a, 3, &inc); // => {2, 3, 4});
+    map(a, 3, &inc);
     printf("%d, %d, %d\n", a[0], a[1], a[2]);
 }
