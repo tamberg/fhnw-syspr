45b48a8ba3f07eef236b8a439e8952759b0e300a
Thomas Amberg <tamberg@yaler.net>
2018-08-21 18:39:42 +0000

    Added map.c

---
+++ 02/map.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int inc(int i) {
+    printf("inc\n");
+    return i + 1;
+}
+
+void map(int a[], int len, int (*f)(int)) {
+    printf("map\n");
+    for (int i=0; i < len; i++) {
+        a[i] = f(a[i]); 
+    }
+}
+
+void map(int a[], int len, int (*f)(int));
+
+int main() {
+    int a[] = {1, 2, 3};
+    map(a, 3, &inc); // => {2, 3, 4});
+    printf("%d, %d, %d\n", a[0], a[1], a[2]);
+}
