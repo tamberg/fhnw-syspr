fbcdba28ec41b3c2cbb5287d1eeffd50d54164d4
Thomas Amberg <tamberg@yaler.net>
2018-08-28 18:33:17 +0000

    Added upper.c

---
+++ 01/upper.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int c = getchar();
+    int result;
+    if (('A' <= c) && (c <= 'Z')) { // for ASCII
+        result = 'a' + c - 'A';
+    } else {
+        result = c;
+    }
+    printf("%c", result);
+    return 0;
+}
