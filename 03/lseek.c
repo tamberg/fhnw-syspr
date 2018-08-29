bda1d2cc1e8f303963433641358f0c1721d8ce03
Thomas Amberg <tamberg@yaler.net>
2018-08-29 08:35:13 +0000

    Fixed read return type. Lseek CUR again to show how it changes.

--- 03/lseek.c
+++ 03/lseek.c
@@ -9,11 +9,13 @@ int main() {
     // TODO: error handling
     char buf[BUF_SIZE];
     int fd = open("./lseek.c", O_RDONLY);
-    int r = read(fd, buf, BUF_SIZE);
+    ssize_t r = read(fd, buf, BUF_SIZE);
     off_t c = lseek(fd, 0, SEEK_CUR);
     printf("%d\n", c);
     off_t s = lseek(fd, 0, SEEK_SET);
     printf("%d\n", s);
     off_t e = lseek(fd, 0, SEEK_END);
     printf("%d\n", e);
+    c = lseek(fd, 0, SEEK_CUR);
+    printf("%d\n", c);
 }
