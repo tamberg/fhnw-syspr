d8b079410ebec29ff1330926c7f1dd4fa1d91cc1
Thomas Amberg <tamberg@yaler.net>
2018-08-29 09:11:57 +0000

    Cleanup.

--- 03/lseek.c
+++ 03/lseek.c
@@ -18,11 +18,11 @@ int main() {
     printf("%d\n", end);
     int len = end - set;
     cur = lseek(fd, 0, SEEK_CUR);
-    printf("%d\n", cur); // min(BUF_SIZE, len)
+    printf("%d\n", cur);
     cur = lseek(fd, -len, SEEK_END);
-    printf("%d\n", cur); // 0
+    printf("%d\n", cur);
     cur = lseek(fd, len, SEEK_SET);
-    printf("%d\n", cur); // end
+    printf("%d\n", cur);
     r = read(fd, buf, BUF_SIZE);
-    printf("%d\n", r); // 0, EOF
+    printf("%d\n", r); // EOF
 }
