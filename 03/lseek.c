756669a1affb3d65ef1cdf274f8d446656738794
Thomas Amberg <tamberg@yaler.net>
2018-08-29 08:25:53 +0000

    Added lseek.c

---
+++ 03/lseek.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUF_SIZE 128 //1024
+
+int main() {
+    // TODO: error handling
+    char buf[BUF_SIZE];
+    int fd = open("./lseek.c", O_RDONLY);
+    int r = read(fd, buf, BUF_SIZE);
+    off_t c = lseek(fd, 0, SEEK_CUR);
+    printf("%d\n", c);
+    off_t s = lseek(fd, 0, SEEK_SET);
+    printf("%d\n", s);
+    off_t e = lseek(fd, 0, SEEK_END);
+    printf("%d\n", e);
+}
