198784269aa7d4a9c276a9862b7f8d0b00e87ed8
Thomas Amberg <tamberg@yaler.net>
2018-08-29 08:49:46 +0000

    Extended lseek.c

--- 03/lseek.c
+++ 03/lseek.c
@@ -10,12 +10,19 @@ int main() {
     char buf[BUF_SIZE];
     int fd = open("./lseek.c", O_RDONLY);
     ssize_t r = read(fd, buf, BUF_SIZE);
-    off_t c = lseek(fd, 0, SEEK_CUR);
-    printf("%d\n", c);
-    off_t s = lseek(fd, 0, SEEK_SET);
-    printf("%d\n", s);
-    off_t e = lseek(fd, 0, SEEK_END);
-    printf("%d\n", e);
-    c = lseek(fd, 0, SEEK_CUR);
-    printf("%d\n", c);
+    off_t cur = lseek(fd, 0, SEEK_CUR);
+    printf("%d\n", cur);
+    off_t set = lseek(fd, 0, SEEK_SET);
+    printf("%d\n", set);
+    off_t end = lseek(fd, 0, SEEK_END);
+    printf("%d\n", end);
+    int len = end - set;
+    cur = lseek(fd, 0, SEEK_CUR);
+    printf("%d\n", cur); // min(BUF_SIZE, len)
+    cur = lseek(fd, -len, SEEK_END);
+    printf("%d\n", cur); // 0
+    cur = lseek(fd, len, SEEK_SET);
+    printf("%d\n", cur); // end
+    r = read(fd, buf, BUF_SIZE);
+    printf("%d\n", r); // 0, EOF
 }
