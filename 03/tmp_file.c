427816c3bfa317dab7adab94ab7f08cdc19716bb
Thomas Amberg <tamberg@yaler.net>
2018-09-04 09:44:29 +0200

    Added erorr handling.

--- 03/tmp_file.c
+++ 03/tmp_file.c
@@ -3,18 +3,28 @@
 #include <unistd.h>
 #include <errno.h>
 
+void error(char *fun_name) {
+    printf("%s: error no %d\n", fun_name, errno);
+    exit(-1);
+}
+
 int main() {
     char template[] = "/tmp/my-XXXXXX";
     int fd = mkstemp(template);
     if (fd == -1) {
-        printf("mkstep: error no %d\n", errno);
-        return -1;
+        error("mkstep");
     }
     printf("created filename %s\n", template);
     printf("press RETURN to unlink\n");
     int ch = getchar();
     unlink(template);
+    if (fd == -1) {
+        error("unlink");
+    }
     printf("unlinked filename %s\n", template);
     close(fd);
+    if (fd == -1) {
+        error("close");
+    }
     return 0;
 }
