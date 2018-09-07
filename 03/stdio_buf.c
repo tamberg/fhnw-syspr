f7482052134274b9a8ba06b35ea7d69203e43655
Thomas Amberg <tamberg@yaler.net>
2018-09-07 16:37:04 +0000

    Added stdio_buf.c

---
+++ 03/stdio_buf.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define BUF_SIZE 1024
+#define N 10000
+
+void error(char *fun_name) {
+    printf("%s: error no %d\n", fun_name, errno);
+    exit(-1);
+}
+
+int main() {
+    char buf[BUF_SIZE];
+    FILE *stream = stdout;
+    errno = 0; // setvbuf may set errno
+    int res = setvbuf(stream, buf, _IOFBF, BUF_SIZE);
+    if (res != 0) { // returns nonzero on failure
+        error("setvbuf");
+    }
+    for (int i = 0; i < N; i++) {
+        res = fprintf(stream, "hello"); 
+        if (res == -1) {
+            error("fprintf");
+        }
+    }
+    return 0;
+}
