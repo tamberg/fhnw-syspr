242edbbf15dfd9c0ee32f7e5d33bdc7f596289d2
Thomas Amberg <tamberg@yaler.net>
2018-09-08 21:05:17 +0000

    Cleanup. Added comments.

--- 03/cpu_temp.c
+++ 03/cpu_temp.c
@@ -7,26 +7,19 @@
 #define BUF_SIZE 32
 
 int main() {
-    //char buf[BUF_SIZE];
+    char buf[BUF_SIZE];
     int fd0 = open("/sys/class/thermal/thermal_zone0/temp", O_RDONLY);
     int fd1 = open("./temp.txt", O_CREAT|O_WRONLY|O_TRUNC|O_APPEND, S_IRUSR|S_IWUSR);
     while (1) {
-        char *buf;
-        buf = malloc(BUF_SIZE);
-        lseek(fd0, 0, SEEK_SET);
-        int r = read(fd0, buf, BUF_SIZE);
-        int i = r - 3 - 1;
-        write(fd1, buf, i);
-        write(fd1, ".", 1);
-        while (i > 0) {
-            buf++;
-            i--;
-        }
-        write(fd1, buf, 3);
-//        for (int j = i; j < i + 3; j++) {
-//            write(fd1, &buf[j], 1);
-//        }
-        write(fd1, ",\r\n", 3); // CSV format ',' CRLF
+        lseek(fd0, 0, SEEK_SET); // set CUR to beginning of file fd0
+        int r = read(fd0, buf, BUF_SIZE); // read, e.g. 37500\n => r = 6
+        int i = r - 3 - 1; // length of integer part of 37.500 => i = 2
+        char *p = buf; // pointer to &buf[0]
+        write(fd1, p, i); // write i chars starting at p => 37
+        p += i; // move p to compensate for written chars
+        write(fd1, ".", 1); // write (missing) point
+        write(fd1, p, 3); // write 3 chars =>  500
+        write(fd1, ",\r\n", 3); // CSV => ,CRLF
         sleep(3); // seconds
     }
     // CTRL-C to exit process, closes fd0, fd1
