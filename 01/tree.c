040658c5771d4625298e0b904bde173e9aacf4df
tamberg <tamberg@yaler.net>
2018-09-23 13:06:37 +0200

    Added free.

--- 01/tree.c
+++ 01/tree.c
@@ -9,29 +9,36 @@ typedef struct node {
 
 int main() {	
 	Node *ll = malloc(sizeof(Node));
-	ll->label = "I am left left";
-	ll->left = NULL;
-	ll->right = NULL;
-	
+    ll->label = "I am left left";
+    ll->left = NULL;
+    ll->right = NULL;
+
 	Node *lr = malloc(sizeof(Node));
-	lr->label = "I am left right";
-	lr->left = NULL;
-	lr->right = NULL;
+    lr->label = "I am left right";
+    lr->left = NULL;
+    lr->right = NULL;
 
-	Node *l = malloc(sizeof(Node));
-	l->label = "I am left";
-	l->left = ll;
-	l->right = lr;
+    Node *l = malloc(sizeof(Node));
+    l->label = "I am left";
+    l->left = ll;
+    l->right = lr;
 
-	Node *r = malloc(sizeof(Node));
-	r->label = "I am right";
-	r->left = NULL;
-	r->right = NULL;
+    Node *r = malloc(sizeof(Node));
+    r->label = "I am right";
+    r->left = NULL;
+    r->right = NULL;
 
-	Node *root = malloc(sizeof(Node));
-	root->label = "I am root";
-	root->left = l;
-	root->right = r;
+    Node *root = malloc(sizeof(Node));
+    root->label = "I am root";
+    root->left = l;
+    root->right = r;
 	
-	return 0;
+    free(n00);
+    free(n01);
+    free(n02);
+    free(n0);
+    free(n1);
+    free(root);
+
+    return 0;
 }
