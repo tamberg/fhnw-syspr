c9257295358d53a495427abeba9a735cc64e416a
tamberg <tamberg@yaler.net>
2018-09-25 10:09:55 +0200

    Renamed struct_fun.c -> struct.c, Added struct_v2.c

---
+++ 02/struct_v2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct { int x; int y; } Point;
+
+Point *create_point(int x, int y) {
+    Point *p = malloc(sizeof(Point));
+    p->x = x;
+    p->y = y;
+    return p;
+}
+
+int main() {
+    Point *origin = create_point(42, 23);
+    printf("(%d, %d)\n", origin->x, origin->y);
+    free(origin);
+    return 0;
+}
