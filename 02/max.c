0fe8756eafdf8b285b07a4d38e9a9e736a5b69c4
Thomas Amberg <tamberg@yaler.net>
2018-09-29 10:29:53 +0200

    Update max.c

--- 02/max.c
+++ 02/max.c
@@ -6,7 +6,7 @@ int max2(int a, int b) { return a > b ? a : b; }
 
 int main() {
     int i = 1, j = 0;
-    int m1 = max1(i++, j);
-    int m2 = max2(i++, j);
+    int m1 = max1(i++, j); // => m1 = ((i++) > (j) ? (i++) : (j)) => 2 x side effect!
+    int m2 = max2(i++, j); // => m2 = max(2, 0)
     printf("m1 = %d, m2 = %d\n", m1, m2);
 }
