b970918b6389048cb728bcfc97b272c82332c8eb
tamberg <tamberg@yaler.net>
2018-09-29 09:56:21 +0000

    Added index to output.

--- 02/errno.c
+++ 02/errno.c
@@ -6,6 +6,6 @@ int main() {
     printf("%d\n", EINTR);
     // $ sudo find / -name errno*.h => ~133
     for (int i = 0; i < 135; i++) {
-        printf("%s\n", strerror(i));
+        printf("%d: %s\n", i, strerror(i));
     }
 }
