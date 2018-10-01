b626c9ac4ea0f2dd0ceb08782273e18c5c75e3fe
tamberg <tamberg@yaler.net>
2018-10-01 15:24:27 +0000

    Updated errno.c

--- 02/errno.c
+++ 02/errno.c
@@ -4,6 +4,10 @@
 
 int main() {
     printf("%d\n", EINTR);
+
+    errno = 27;
+    perror("open"); // reads errno
+
     // $ sudo find / -name errno*.h => ~133
     for (int i = 0; i < 135; i++) {
         printf("%d: %s\n", i, strerror(i));
