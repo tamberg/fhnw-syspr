406ce90ba625c984006088293c4296ab432accc0
tamberg <tamberg@yaler.net>
2018-10-10 22:26:23 +0200

    Cleanup.

--- 04/environ.c
+++ 04/environ.c
@@ -7,23 +7,36 @@
 
 extern char **environ;
 
-int main() {
+void print_environ() {
     char** p = environ;
-    while (*p != 0) {
+    while (*p != NULL) {
         printf("%s\n", *p);
-	p++;
+        p++;
     }
+}
 
+void setenv_const() {
+    int overwrite = 0; // don't overwrite the value if the name exists
+    setenv("MY_CONST_VAR", "hola", overwrite); // requires _BSD_SOURCE
+}
+
+void putenv_heap() {
     const int N = 18;
     char *my_var = (char *) malloc(N * sizeof(char));
     strcpy(my_var, "MY_HEAP_VAR=hello");
-    putenv(my_var);
+    putenv(my_var); // string must be heap allocated
+}
 
-    int overwrite = 0; // don't overwrite the value if the name exists
-    setenv("MY_CONST_VAR", "hola", overwrite); // requires _BSD_SOURCE
-
+void getenv_both() {
     char *h = getenv("MY_HEAP_VAR");
     printf("MY_HEAP_VAR=%s\n", h);
     char *c = getenv("MY_CONST_VAR");
     printf("MY_CONST_VAR=%s\n", c);
+}
+
+int main() {
+    print_environ();
+    setenv_const();
+    putenv_heap();
+    getenv_both();
 }
