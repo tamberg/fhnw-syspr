e111336cbad181d77cc8fa2d3b64d107057a0b63
tamberg <tamberg@yaler.net>
2018-10-11 17:42:53 +0200

    Split off break.c

--- 04/heap.c
+++ 04/heap.c
@@ -1,23 +1,15 @@
-#define _XOPEN_SOURCE 500
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 int main() {
     int n = 32;
     int size = n * sizeof(int);
     int *p = (int *) malloc(size);
+    int *p0 = p;
     while (n > 0) {
+        printf("%p, %d\n", (void *) p, *p); // garbage, if reused
         p++;
         n--;
     }
-
-    void *b0 = sbrk(0);
-    printf("current program break is at %p\n", b0);
-    b0 = (char*) b0 + 1;
-    printf("%p is after the program break\n", b0);
-    brk(b0);
-    void *b1 = sbrk(0);
-    printf("new program break is at %p\n", b1);
+    free(p0);
 }
