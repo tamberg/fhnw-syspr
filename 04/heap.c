73e3e3a4a2d9991c85aed5bc38ee3beeff43650d
tamberg <tamberg@yaler.net>
2018-10-11 22:32:40 +0000

    Added size.

--- 04/heap.c
+++ 04/heap.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 32
+#define N 4
 
 int main() {
     int size = N * sizeof(int);
     int *p = (int *) malloc(size);
     int *p0 = p;
+    //printf("%d\n", *(--p)); p++;
     for (int i = 0; i < N; i++) {
         printf("%p, %d\n", (void *) p, *p); // garbage, if reused
         p++;
