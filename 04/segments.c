9a4b3260b4e6803cda0a80b32a1d842083309561
tamberg <tamberg@yaler.net>
2018-10-11 18:01:51 +0200

    Cleanup.

--- 04/segments.c
+++ 04/segments.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
-#include <unistd.h>
 
-extern char etext, edata, end;
+extern char etext, edata, end; // no include required
 
+int i; // uninitialised data
 char a[1024]; // uninitialised data
 char b[] = {0x00, 0x01, 0x02}; // initialised data
 
-void f(void) { // stack frame for f()
+void f(void) {
     char c[4096]; // stack frame for f()
     char d[] = {0x00}; // stack frame for f()
 }
 
+void g(void) {
+    char e[256]; // stack frame for g()
+    char *s = "hello"; // stack frame for g()
+}
+
 int main() {
-    printf("PID = %d\n", getpid());
     printf("&etext = %p\n", (void *) &etext);
     printf("&edata = %p\n", (void *) &edata);
     printf("&end = %p\n", (void *) &end);
