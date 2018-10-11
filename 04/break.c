292c9ee7d481d8c2a4e2f22a0943e55d266ee99b
tamberg <tamberg@yaler.net>
2018-10-11 17:42:32 +0200

    Added break.c

---
+++ 04/break.c
@@ -0,0 +1,14 @@
+#define _XOPEN_SOURCE 500
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    void *b0 = sbrk(0);
+    printf("current program break is at %p\n", b0);
+    b0 = (char*) b0 + 1;
+    printf("%p is after the program break\n", b0);
+    brk(b0);
+    void *b1 = sbrk(0);
+    printf("new program break is at %p\n", b1);
+}
