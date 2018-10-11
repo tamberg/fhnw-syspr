900d008f37be6209ae8eb7c8bf1c13af9c0bb4dc
tamberg <tamberg@yaler.net>
2018-10-11 23:08:31 +0000

    Cleanup.

--- 04/heap.c
+++ 04/heap.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 4
+#define N 8
 
 int main() {
     int size = N * sizeof(int);
     int *p = (int *) malloc(size);
     int *p0 = p;
-    //printf("%d\n", *(--p)); p++;
     for (int i = 0; i < N; i++) {
         printf("%p, %d\n", (void *) p, *p); // garbage, if reused
         p++;
