906bbbf520b403a8a70ec7cd024cf92fdab8821a
tamberg <tamberg@yaler.net>
2018-10-13 16:00:04 +0000

    Cleanup.

--- 04/sigint.c
+++ 04/sigint.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <signal.h>
-//#include <unistd.h>
+#include <unistd.h>
 
 int flag = 0;
 
 void handle(int signal) {
-    printf("handling signal ", signal);
+    printf("handling signal %d\n", signal);
 }
 
 int main() {
     signal(SIGINT, handle);
-    while (true) {
+    while (1) {
         sleep(3);
     }
 }
