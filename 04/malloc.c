88da87c3fd326de8f519d66b3df650f9e952ef06
tamberg <tamberg@yaler.net>
2018-10-15 10:15:48 +0200

    Added malloc.c

--- 04/malloc.c
+++ 04/malloc.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+int _size;
 
 void *my_malloc(size_t size) {
-    return malloc(size); // TODO: replace
+    _size = size;
+    void *p = sbrk(size);
+    return p;
 }
 
 void my_free(void *p) {
-    free(p); // TODO: replace
+    sbrk(_size);
 }
 
 int main() {
     int n = 3;
     int *p = my_malloc(n * sizeof(int));
     for (int i = 0; i < n; i++) {
-        printf("%p: %d\n", p, *p);
+        printf("%p: %d\n", (void *) p, *p);
         p++;
     }
     p -= n;
