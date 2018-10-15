a10450b11519de3113261f67442a89be40cda87f
Thomas Amberg <tamberg@yaler.net>
2018-10-15 13:09:42 +0200

    Update heap.c

--- 04/heap.c
+++ 04/heap.c
@@ -5,7 +5,7 @@
 
 int main() {
     int size = N * sizeof(int);
-    int *p = (int *) malloc(size);
+    int *p = malloc(size);
     int *p0 = p;
     for (int i = 0; i < N; i++) {
         printf("%p, %d\n", (void *) p, *p); // garbage, if reused
