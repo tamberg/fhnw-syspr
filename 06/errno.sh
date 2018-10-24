1454f7983adb8ad836a5ed8f5d3ab48be75e1a18
tamberg <tamberg@yaler.net>
2018-10-24 13:11:17 +0000

    Cleanup.

--- 06/errno.sh
+++ 06/errno.sh
@@ -3,7 +3,7 @@
 # $ chmod a+x errno.sh
 # $ ./errno.sh
 
-echo "> expand, compile and grep errno.h:" &&
+echo "expand, compile and grep errno.h:" &&
 echo '#include <errno.h>' | gcc -pthread -E - | grep errno &&
-echo "> take a closer look:" &&
+echo "take a closer look:" &&
 cat /usr/include/arm-linux-gnueabihf/bits/errno.h | grep errno
