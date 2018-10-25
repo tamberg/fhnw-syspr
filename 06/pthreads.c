c5a3d467288665fe08738fd3dd01092caa38e01f
tamberg <tamberg@yaler.net>
2018-10-25 18:21:33 +0000

    pdated pthreads.c

--- 06/pthreads.c
+++ 06/pthreads.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void *run(void *arg) {
-    printf("run\n");
+void *start(void *arg) {
+    printf("start\n");
     printf("arg = %d\n", *((int *) arg));
-    pthread_exit(arg);
+    pthread_t id = pthread_self();
+    printf("id = %lu\n", id);
+    pthread_exit((void *) 0);
 }
 
 int main() {
     printf("main\n");
     pthread_t thread;
-    int i = 42;
-    pthread_create(&thread, NULL, run, &i);
-    void *status;
-    pthread_join(thread, &status);
-    printf("status = %d\n", *((int *) status));
+    int start_arg = 42;
+    pthread_create(&thread, NULL, start, &start_arg);
+    void *start_res;
+    pthread_join(thread, &start_res);
+    printf("thread = %lu\n", thread);
+    printf("result = %d\n", (int) start_res);
 }
