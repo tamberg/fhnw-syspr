572e2f196b3d020f8b07b88fca521c69aa901b3a
tamberg <tamberg@yaler.net>
2020-03-01 12:38:16 +0100

    Renamed files.

--- 02/nest/heater.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-#define MIN_TEMP 7
-#define MAX_TEMP 42
-
-int heater_temp(void);
-void heater_up(void);
-void heater_down(void);
--- 02/nest/nest.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-#define MIN_TEMP 7
-#define MAX_TEMP 42
-
-int nest_temp(void);
-void nest_up(void);
-void nest_down(void);
