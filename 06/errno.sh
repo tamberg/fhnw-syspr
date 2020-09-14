38b4d22e7b9ae3822875a97fffd404f452c0d217
tamberg <tamberg@yaler.net>
2020-09-14 22:09:43 +0200

    Moved directories.

--- 06/errno.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# $ chmod a+x errno.sh
-# $ ./errno.sh
-
-set -x && # show commands, https://serverfault.com/a/16208/3827
-echo '#include <errno.h>' | gcc -pthread -E - | grep errno &&
-cat /usr/include/arm-linux-gnueabihf/bits/errno.h | grep errno
