52bf095d539f7d3c63b6b3768a8a598bf8aebc2e
tamberg <tamberg@yaler.net>
2021-02-23 09:55:04 +0000

    Fix typo.

--- 01/arrays.c
+++ 01/arrays.c
@@ -4,7 +4,7 @@ int main() {
     //float values[3];
     float values[3] = { 20.1, 23, 15.2 };
     printf("%.1f\n", values[1]);
-    values[1] =  7.0;
+    values[1] = 7.0;
     printf("%.1f\n", values[2]);
     int n = sizeof(values) / sizeof(values[0]);
     printf("%d\n", n);
