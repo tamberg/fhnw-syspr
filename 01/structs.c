5f63a35b1fd77ea309624afb3a9109853455f1b8
tamberg <tamberg@yaler.net>
2021-09-22 18:47:38 +0100

    Update.

--- 01/structs.c
+++ 01/structs.c
@@ -5,6 +5,7 @@ int main(void) {
     struct point p = { 3, 2 };
     struct point q;
     q.x = p.y;
+    q.y = 0;
     printf("(%d, %d)\n", p.x, p.y);
     printf("(%d, %d)\n", q.x, q.y);
     return 0;
