c8601be4639c6c094f1b1f198b9c93cebc884b4c
Thomas Amberg <tamberg@yaler.net>
2021-11-05 00:29:53 +0100

    Update prod.c

--- 06/prod.c
+++ 06/prod.c
@@ -1,42 +1,40 @@
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include <unistd.h>
 
-pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+#define STOCK_MAX 32
 
-int n = 10; // max number of items
-int i = 0; // shared resource
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+volatile int stock = 0;
 
 void *produce(void *arg) {
     while (1) {
-        pthread_mutex_lock(&m);
-        if (i < n) { // not full
-            i++; // produce
-            printf("produced one item, i = %d\n", i);
-        } else {
-            printf("produced nothing.\n");
+        pthread_mutex_lock(&m); // blocking
+        // critical section >>
+        if (stock < STOCK_MAX) {
+            stock++;
         }
+        printf("producer, stock = %d\n", stock);
+        // << critical section
         pthread_mutex_unlock(&m);
-        sleep(5); // s
     }
 }
 
 void *consume(void *arg) {
     while (1) {
-        pthread_mutex_lock(&m);
-        if (i > 0) { // not empty
-            i--; // consume
-            printf("consumed one item, i = %d\n", i);
-        } else {
-            printf("consumed nothing.\n");
+        pthread_mutex_lock(&m); // blocking
+        // critical section >>
+        if (stock > 0) {
+            stock--;
         }
+        printf("consumer, stock = %d\n", stock);
+        // << critical section
         pthread_mutex_unlock(&m);
-        sleep(1); // s
     }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     pthread_t t1;
     pthread_t t2;
     pthread_create(&t1, NULL, produce, NULL);
