1b8db7837b390d5c196418d909ed42eaefba9182
tamberg <tamberg@yaler.net>
2022-03-17 11:43:53 +0100

    Add example.

---
+++ 04/impl.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <malloc.h>
+
+int main(void) {
+    size_t n = sizeof(int);
+    int *p = malloc(n);
+    // rounds size_t n for alignment,
+    // stores value to the left of p,
+    // see also TLPI p. 144 ff.
+    size_t s = *(((size_t *) p) - 1);
+    size_t u = malloc_usable_size(p);
+    printf("%d ?= %d ?= %d\n", n, s, u);
+    return 0;
+}
+
