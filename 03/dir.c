b1afde080daa1ec2e13203b7a70787bc97011378
Thomas Amberg <tamberg@yaler.net>
2022-10-18 13:16:53 +0200

    Update dir.c

--- 03/dir.c
+++ 03/dir.c
@@ -4,27 +4,25 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-void error(char *fun_name) {
-    printf("%s: error no %d\n", fun_name, errno);
-    exit(-1);
-}
-
 int main(void) {
     //const char *path = "./my_dir/my_sub_dir";
     const char *path = "./my_dir";
     mode_t mode = S_IRUSR | S_IWUSR | S_IXUSR;
     int result = mkdir(path, mode);
     if (result == -1) {
-        error("mkdir");
+        perror("mkdir");
+        return -1;
     }
     const char *path2 = "my_renamed_dir";
     result = rename(path, path2);
     if (result == -1) {
-        error("rename");
+        perror("rename");
+        return -1;
     }
     result = remove(path2);
     if (result == -1) {
-        error("remove");
+        perror("remove");
+        return -1;
     }
     return 0;
 }
