6fb6ada50fb9983706dc82d8dc22a914cb56d1f9
tamberg <tamberg@yaler.net>
2023-03-17 11:54:55 +0100

    Add example.

---
+++ 03/read.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        perror("open"); // reads errno
+        return -1;
+    }
+    int n = 32;
+    char buf[n]; // allocated on stack
+    ssize_t r = read(fd, buf, n);
+    while (r != 0) {
+        write(STDOUT_FILENO, buf, r);
+        r = read(fd, buf, n);
+    }
+    if (r == -1) {
+        perror("read"); // reads errno
+        return -1;
+    }
+    close(fd);
+    return 0;
+}
