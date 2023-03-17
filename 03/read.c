94f7e174a06696ccf50bbba8d942cc6891d2f000
tamberg <tamberg@yaler.net>
2023-03-17 11:57:59 +0100

    Update.

--- 03/read.c
+++ 03/read.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -11,13 +12,15 @@ int main(int argc, char *argv[]) {
     int n = 32;
     char buf[n]; // allocated on stack
     ssize_t r = read(fd, buf, n);
-    while (r != 0) {
+    while (r > 0) {
         write(STDOUT_FILENO, buf, r);
         r = read(fd, buf, n);
     }
     if (r == -1) {
         perror("read"); // reads errno
         return -1;
+    } else {
+        assert(r == 0); // EOF
     }
     close(fd);
     return 0;
