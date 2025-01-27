2685fc09091b3d238582332db1271a3b2ca682c0
Thomas Amberg <tamberg@yaler.net>
2025-01-27 19:00:41 +0100

    Update sizeof.c

--- 01/sizeof.c
+++ 01/sizeof.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("sizeof(char) = %lu\n", sizeof(char));
-    printf("sizeof(short) = %lu\n", sizeof(short));
-    printf("sizeof(int) = %lu\n", sizeof(int));
-    printf("sizeof(unsigned int) = %lu\n", sizeof(unsigned int));
-    printf("sizeof(long) = %lu\n", sizeof(long));
-    printf("sizeof(unsigned long) = %lu\n", sizeof(unsigned long));
-    printf("sizeof(long long) = %lu\n", sizeof(long long));
-    printf("sizeof(float) = %lu\n", sizeof(float));
-    printf("sizeof(double) = %lu\n", sizeof(double));
+    printf("sizeof(char) = %zu\n", sizeof(char));
+    printf("sizeof(short) = %zu\n", sizeof(short));
+    printf("sizeof(int) = %zu\n", sizeof(int));
+    printf("sizeof(unsigned int) = %zu\n", sizeof(unsigned int));
+    printf("sizeof(long) = %zu\n", sizeof(long));
+    printf("sizeof(unsigned long) = %zu\n", sizeof(unsigned long));
+    printf("sizeof(long long) = %zu\n", sizeof(long long));
+    printf("sizeof(float) = %zu\n", sizeof(float));
+    printf("sizeof(double) = %zu\n", sizeof(double));
     return 0;
 }
